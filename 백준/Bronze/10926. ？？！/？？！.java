0ac6815769505a4974baa1474b39bdec244e3c02
mmm307955 <mmm307955@gmail.com>
2025-03-18 19:36:46 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //입력을 받는다. 
+        Scanner sc = new Scanner(System.in);
+        String a = "";
+        a = sc.nextLine();
+        
+        a += "??!";
+        System.out.println(a);
+        
+    }
+}
