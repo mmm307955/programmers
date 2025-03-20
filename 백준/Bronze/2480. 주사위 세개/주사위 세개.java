3a6ccdeda74190d72fe8887311f3b20132e35958
mmm307955 <mmm307955@gmail.com>
2025-03-21 05:11:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -3,35 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int prize = 0;
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int price = 0;
 
-        if(a == b){
-            if(b != c){
-                prize = 1000 + (a * 100);
-            }else{
-                prize = 10000 + (a * 1000);
-            }
-        }else if(b == c){
-            if(a != c){
-                prize = 1000 + (b * 100);
-            }else{
-                prize = 10000 + (a * 1000);
-            }
-        }else if(a == c){
-            if(a != b){
-                prize = 1000 + (a * 100);
-            }else{
-                prize = 10000 + (a * 1000);
-            }
-        }else{
-           int max = Math.max(a,b);
-           max = Math.max(max,c);
-           prize = max * 100;
+        if(A == B && B ==C){
+            price = 10000 + A * 1000;
         }
 
-        System.out.println(prize);
+        else if(A == B){
+            price = 1000 + A * 100;
+        }
+
+        else if(A == C){
+            price = 1000 + A * 100;
+        }
+
+        else if(C == B){
+            price = 1000 + B * 100;
+        }
+
+        else{
+            int max = Math.max(A, B);
+            max = Math.max(max, C);
+            price = max * 100;
+        }
+
+        System.out.println(price);
     }
 }
