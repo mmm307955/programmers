c9f042da51665ac89f7bb239997a82bfb9413ad8
mmm307955 <mmm307955@gmail.com>
2025-03-23 03:01:27 +0900

    [Bronze V] Title: X보다 작은 수, Time: 172 ms, Memory: 17296 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,18 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int X = sc.nextInt();
+    public static void main(String[] args) throws IOException{
 
-        int[] arr = new int[N];
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 첫 줄 입력 → "10 5" 같은 거
+        String[] firstLine = br.readLine().split(" ");
+        int N = Integer.parseInt(firstLine[0]);
+        int X = Integer.parseInt(firstLine[1]);
+
+        // 둘째 줄 입력 → "1 10 4 9 ..." 같은 숫자들
+        String[] secondLine = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(secondLine[i]);
+            if (num < X) {
+                bw.write(num + " ");
+            }
         }
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i] < X) System.out.print(arr[i] + " ");
-        }
+
+        // 출력 마무리
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
