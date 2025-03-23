1f1fef6b40945f2e15bba6f709a554044888b95f
mmm307955 <mmm307955@gmail.com>
2025-03-23 21:45:44 +0900

    [Bronze III] Title: 최소, 최대, Time: 460 ms, Memory: 106352 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
 
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-
+        
+        String[] input = br.readLine().split(" ");
         for(int i = 0; i < N; i++){
-            int num = sc.nextInt();
+            int num = Integer.parseInt(input[i]);
             if (num > max) max = num;
             if(num < min) min = num;
         }
 
-        System.out.println(min + " " + max);
+        bw.write(min + " " + max);
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
