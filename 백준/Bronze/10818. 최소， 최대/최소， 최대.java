c526c65f56f0a12874ab4b9822e26f1954d55600
mmm307955 <mmm307955@gmail.com>
2025-03-23 21:29:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 1600 ms, Memory: 311240 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,20 +4,14 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N];
 
-        arr[0] = sc.nextInt();
-        int max = arr[0];
-        int min = arr[0];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        for(int i = 1; i < N; i++){
-            arr[i] = sc.nextInt();
-            if(arr[i] > max){
-                max = arr[i];
-            }
-            if(arr[i] < min){
-                min = arr[i];
-            }
+        for(int i = 0; i < N; i++){
+            int num = sc.nextInt();
+            if (num > max) max = num;
+            if(num < min) min = num;
         }
 
         System.out.println(min + " " + max);
