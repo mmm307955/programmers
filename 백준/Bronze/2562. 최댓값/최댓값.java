86767b6058c15034ce57269fd0dc625287956080
mmm307955 <mmm307955@gmail.com>
2025-03-24 01:59:14 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 16064 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,20 +1,23 @@
 import java.io.*;
-import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException{
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int max = Integer.MIN_VALUE;
         int index = -1;
+
         for(int i = 0; i < 9; i++){
-            int num = sc.nextInt();
+            int num = Integer.parseInt(br.readLine());
             if(num > max) {
                 max = num;
                 index = i;
             }
         }
-        System.out.println(max);
-        System.out.println(index+1);
+        bw.write(max + "\n" + (index+1) );
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
