d62dc7a466d4ba8adda441ea5c4a81a6d1feb3d1
mmm307955 <mmm307955@gmail.com>
2025-03-31 03:35:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,9 +5,8 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int count = 0;
-        
         while(N >= 0){
-            if(N%5 == 0){
+            if ( N % 5 == 0){
                 count += N/5;
                 System.out.println(count);
                 return;
