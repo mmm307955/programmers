7cd11f20612c1af9426bf01baf07d69617c5a4eb
mmm307955 <mmm307955@gmail.com>
2025-04-07 23:58:35 +0900

    [Bronze I] Title: 단어 공부, Time: 344 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,35 +1,36 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String answer = "?";
+        //일단 String으로 문장을 받고, 받은 걸 대문자로 바꾼다. 
+        //그리고 각 문자의 개수를 세기 위해 char[]로 바꾼다. 
         String str = br.readLine();
-        char[] c = str.toUpperCase().toCharArray();
-        
-        int count = 0;
-        int max = 0;
+        char[] ch = str.toUpperCase().toCharArray();
+        String answer = "?";
         
         HashMap<Character, Integer> map = new HashMap<>();
-        for(char x : c){
-            if(!map.containsKey(x)) count = 1;
-            else{
+        int count = 0;
+        
+        //각 배열을 순회하며 알파벳 개수를 센다. 
+        for(char x : ch){
+            if(!map.containsKey(x)){
+                count = 1;
+            }else{
                 count = map.get(x) + 1;
             }
             map.put(x,count);
         }
         
+        int max = 0;
         for(char x : map.keySet()){
-            if(map.get(x) > max){
+            if(map.get(x) > max) {
                 max = map.get(x);
                 answer = String.valueOf(x);
-            }else if(map.get(x) == max){
-                answer = "?";
+            }
+            else if(map.get(x) == max) answer = "?";
         }
-        
-    }
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
