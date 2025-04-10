bb2fb47f9c265e133b7e6c98a8e10e1bd39bd54b
mmm307955 <mmm307955@gmail.com>
2025-04-10 10:27:37 +0900

    [Gold III] Title: 나머지 합, Time: 492 ms, Memory: 120676 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] sum = new long[N];
+        long[] mod = new long[M];
+        long answer = 0;
+        
+        st = new StringTokenizer(br.readLine());
+        sum[0] = Long.parseLong(st.nextToken());
+        for(int i = 1; i < N; i++){
+            sum[i] = (sum[i-1] + Long.parseLong(st.nextToken()))%M;
+        }
+        for(int i = 0; i < N; i++ ){
+            if(sum[i] % M == 0) answer++;
+            mod[(int)sum[i] % M]++;
+        }
+        
+        for(int i = 0; i < M; i++){
+            if(mod[i] > 1){
+                answer += (mod[i] * (mod[i]-1))/2;
+            }
+        }
+        System.out.println(answer);
+    }
+}
