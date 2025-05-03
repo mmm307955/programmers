03e3367c3ad9fb8afbf477f866c94205bada57fd
mmm307955 <mmm307955@gmail.com>
2025-05-03 15:48:52 +0900

    [Bronze I] Title: 소인수분해, Time: 132 ms, Memory: 14668 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 2; i <= N; i++){
+            while(N%i == 0){
+                N /= i;
+                System.out.println(i);
+            }
+        }
+        br.close();
+    }
+}
